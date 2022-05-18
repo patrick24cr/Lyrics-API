254a8b1f5e8f3055af0756acae597797e93e8054
Patrick Burns <burns212@gmail.com>
2022-05-17 21:19:53 -0500

    basic functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,49 @@
 // USE WITH FIREBASE AUTH
 // import checkLoginStatus from './helpers/auth';
+import axios from 'axios';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
+const getLyrics = (artist, song) => new Promise((resolve, reject) => {
+  axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
+
 const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
+  document.querySelector('#input').innerHTML = `
+    <h1>Lyrics API request</h1>
+    <small>Enter an artist and song name</small><br />
+    <form>
+    <div class="row g-3 user-input">
+    <div class="col">
+    <input type="text" class="form-control" placeholder="Artist" aria-label="Artist" id='artist'>
+    </div>
+    <div class="col">
+    <input type="text" class="form-control" placeholder="Song name" aria-label="Song name" id='song'>
+    </div>
+    </div>
+    <button class="btn btn-danger" type='submit' id="click-me">Request</button><br />
+    </form>
     <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
   `;
-  console.warn('YOU ARE UP AND RUNNING!');
 
   document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
+    .querySelector('form')
+    .addEventListener('submit', (e) => {
+      e.preventDefault();
+      getLyrics(document.querySelector('#artist').value, document.querySelector('#song').value)
+        .then((response) => {
+          document.querySelector('#output').innerHTML = response.lyrics;
+        });
+    });
 
   // USE WITH FIREBASE AUTH
   // checkLoginStatus();
+
+  // getLyrics('beatles', 'michelle').then((response) => {
+  //   document.querySelector('#output').innerHTML = response.lyrics;
+  // });
 };
 
 init();
